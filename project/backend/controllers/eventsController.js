c6e9ab3dec38f9546afe0c8dfca17f5ecca95389
Devthainakarin <thainakarindev@gmail.com>
2024-09-18 10:01:25 +0700

    วันที่ตอบกลับ event report

--- project/backend/controllers/eventsController.js
+++ project/backend/controllers/eventsController.js
@@ -241,6 +241,7 @@ exports.rePort = async (req, res) => {
       o.safety,
       o.service,
       o.management,
+      e.acceptAt,
       CASE WHEN d.name = u.dep THEN 'SR' ELSE 'NR' END AS selfreport,
       CASE WHEN DATEDIFF(hour, o.occurrencedate, o.createAt) < 24 THEN 1  ELSE 0 END AS timelyreport
     FROM ${DB_NAME}.[dbo].[event_logs] e
