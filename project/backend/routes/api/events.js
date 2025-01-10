c19527225cf6892f7da6584d47a9e26ba0559f91
Devthainakarin <thainakarindev@gmail.com>
2025-01-10 18:07:55 +0700

    unlock access add evnet

--- project/backend/routes/api/events.js
+++ project/backend/routes/api/events.js
@@ -7,7 +7,7 @@ const verifyRoles = require("../../middleware/verifyRoles");
 // Get Report with optional startdate and enddate
 router.get('/reports/:startdate?/:enddate?', verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.User), eventsController.rePort);
 
-router.post('/', verifyRoles(ROLES_LIST.Editor, ROLES_LIST.Admin), eventsController.createEventtLog);
+router.post('/', verifyRoles(ROLES_LIST.User, ROLES_LIST.Editor, ROLES_LIST.Admin), eventsController.createEventtLog);
 // router.get('/', verifyRoles(ROLES_LIST.User, ROLES_LIST.Editor, ROLES_LIST.Admin), eventsController.getAllEventLogs);
 router.get('/:reportid', verifyRoles(ROLES_LIST.User, ROLES_LIST.Editor, ROLES_LIST.Admin), eventsController.getAllEventLogs);
 router.get('/:reportid/:id', verifyRoles(ROLES_LIST.User, ROLES_LIST.Editor, ROLES_LIST.Admin), eventsController.getEventLogById);
