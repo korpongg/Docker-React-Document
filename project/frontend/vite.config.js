a012ac00267a1d2741e96ee45aa624b0bc4bddab
Devthainakarin <thainakarindev@gmail.com>
2025-01-31 15:36:24 +0700

    vite config

--- project/frontend/vite.config.js
+++ project/frontend/vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      // Exclude the 'storage' directory from being removed during the build
+      exclude: ['storage/**'],
+    },
+  },
   plugins: [react()],
   server: {
     port: 3000,
